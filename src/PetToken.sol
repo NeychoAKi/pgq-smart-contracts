b12ff58d2608f85a6a391e59f74c07849565e853
Neycho <11023612+neychoaki@user.noreply.gitee.com>
2024-11-22 20:12:24 +0800

    feats: add openzeppelin lib.

--- src/PetToken.sol
+++ src/PetToken.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract PetToken is ERC20("PetToken", "PET"), Ownable(msg.sender) {
     
